8bf348d0bcbd0b4c49a24389e3359b4b4d3dfac6
Pedro-alves-arruda <pedro.alves.arruda10@gmail.com>
2025-09-26 11:41:26 -0300

    first commit

---
+++ TokenServices/src/test/java/io/github/pedroalvesarruda/TokenServices/TokenServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedroalvesarruda.TokenServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
